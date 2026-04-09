61ce9995dbfa481a5b06fb27dd824b9f1bd55905
Diefly Oskar <105210849+DieflyCHN@users.noreply.github.com>
2026-04-09 17:59:44 +0200

    Update market simulation with config and input fixes
    Refactor market simulation to use configuration for max ticks and improve input handling.

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python
-from market_simulation.market_simulator import MarketIndex
 from plot import plot_result
+from market_simulation.market_simulator import MarketIndex
 from trady import signal_buy_chasing, signal_buy_lowBuild, signal_sell
 from account import Account
+from config import MAIN_CONFIG
+CFG = MAIN_CONFIG
 
 mode = input("""Please select your stock market beginning status:
     (U): bUll market
     (R): beaR market
     (F): high Fluctuation market      
-    :""").upper()
+    :""").strip().upper()
 
 while mode not in ["U", "R", "F"]:
     mode = input ("INCORRECT mode selection, please select again:").upper()
 
-market_index = MarketIndex(mode)
+market_index = MarketIndex(mode)    # obserable Index (for trader)
 account = Account()
 
-# Setup simulation ticks (How many cycles?)
-while market_index.tick < 10000:
+while market_index.tick < CFG.MAX_TICKS:
     current_price = market_index.current_price
-    
+  
+    # Selling Strategy
+    orders = signal_sell(account, current_price)
+
+    for order in sorted(orders, key=lambda x: x["position_num"], reverse=True):
+        account.sell(current_price, order["position_num"], order["shares_to_sell"])
+
+    # Buying Strategy (Switch Option not finished, use it by hand)
     # if signal_buy_lowBuild(market_index.down_streak):
     if signal_buy_chasing(market_index.up_streak):
         account.buy(current_price)
 
-    strategy, position_num = signal_sell(account, market_index.current_price)
-    if strategy is not False:
-        account.sell(current_price, position_num, strategy)
-
-    account.update_book_value(market_index.current_price)
+    account.update_position()
+    account.update_book_value(current_price)
 
     market_index.next_tick()
 
