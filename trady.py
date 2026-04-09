99a3fd86994b2c159f309485f164ab0fbb4cb53a
Diefly Oskar <105210849+DieflyCHN@users.noreply.github.com>
2026-04-09 18:00:05 +0200

    Refactor buy/sell signals and update logic

--- trady.py
+++ trady.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python
+from config import TRADY_CONFIG
+CFG = TRADY_CONFIG
 
 def signal_buy_lowBuild(down_streak):
     """
     Buy rule:
     If `down_streak >= 5`, buy using 50% of current cash.
-    This rule will repeatedly trigger during continuous downward trends,
+    This rule will `repeatedly` trigger during continuous downward trends,
     resulting in progressively smaller position entries (geometric scaling).
     If you dont like it, use `down_streak == 5`.
     """
-    return down_streak >= 5
+    return down_streak == 5
 
 def signal_buy_chasing(up_streak):
-    return up_streak >= 5
+    return up_streak == 5
 
 def signal_sell(account, current_price):
-    half_candidate = None
+    orders = []
 
     for i, pos in enumerate(account.positions):
         rate = (current_price - pos["entry_price"]) / pos["entry_price"]
-        
-        if rate >= 0.1 or rate <= -0.05:
-            return "full", i
-        elif rate >= 0.05 and half_candidate is None:
-            half_candidate = i
-        
-    if half_candidate is not None:
-        return "half", half_candidate
+        shares_to_sell = 0
+
+        if rate >= (CFG.PROFIT_PCT / 100) or rate <= (CFG.STOP_LOSS_PCT / 100):
+            shares_to_sell = pos["shares"]
+        elif rate >= (CFG.PARTIAL_PROFIT_PCT):
+            # Sell a part of all shares
+            shares_to_sell = int(pos["shares"] * CFG.PARTIAL_SHARES_PCT / 100)
+            # when only have 1 share, it will become 0, so just sell it
+            if shares_to_sell == 0:
+                shares_to_sell = 1
+
+        # check if it really wants to sell
+        if shares_to_sell > 0:
+            # Record the position number and shares to be sold
+            orders.append({"position_num": i, "shares_to_sell": shares_to_sell})
     
-    return False, None
-    
+    return orders
