98201c071ee596765bb07d167b88d53200226bdb
Diefly Oskar <105210849+DieflyCHN@users.noreply.github.com>
2026-04-09 18:00:21 +0200

    Refactor Account class to use config values

--- account.py
+++ account.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+from config import ACCOUNT_CONFIG
+CFG = ACCOUNT_CONFIG
+
 class Account:
     def __init__(self):
-        self.cash = 10000
+        self.cash = CFG.START_CASH
         self.positions = []
+        # Book value: cash + stock value
         self.book_value = self.cash
         self.book_value_history = [self.book_value]
     
@@ -14,7 +18,7 @@ class Account:
         # print(f"Book value: ${self.book_value:.2f}")
 
     def buy(self, current_price):
-        amount = self.cash * 0.5
+        amount = self.cash * CFG.BUY_PCT / 100
         shares = int(amount / current_price)
         if shares < 1:
             return False
@@ -25,38 +29,25 @@ class Account:
             print(f"Cash now: {self.cash:.2f}")
             return True
 
-    def sell(self, current_price, position_num, strategy):
+    def sell(self, current_price, position_num, shares_to_sell):
         if not self.positions:
             return False
         if position_num < 0 or position_num >= len(self.positions):
             return False
         
         this_position = self.positions[position_num]
-        sell_count = 0
-        if strategy == "full":
-            sell_shares = this_position["shares"]
-
-            self.cash += sell_shares * current_price
-            sell_count += sell_shares
-            this_position["shares"] = 0
-
-        elif strategy == "half":
-            sell_shares = int(this_position["shares"] * 0.5)
-            # When only have 1 share, just sell it.
-            if sell_shares == 0:
-                sell_shares = 1
-
-            self.cash += sell_shares * current_price
-            this_position["shares"] -= sell_shares
-            sell_count += sell_shares
-        else:
-            return False
+        self.cash += shares_to_sell * current_price
         
+        if shares_to_sell <= this_position["shares"]:
+            this_position["shares"] -= shares_to_sell
+
+        print(f"Sold shares: {shares_to_sell} at price ${current_price}")
+        
+        return shares_to_sell > 0                        
+    
+    def update_position(self):
+        # To prevent the "0" share position from continuing to exist
         self.positions = [pos for pos in self.positions if pos["shares"] > 0]
 
-        print(f"Sold shares: {sell_count} at price ${current_price}")
-        
-        return sell_count > 0
-    
     def has_position(self):
         return len(self.positions) > 0
