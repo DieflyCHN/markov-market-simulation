c03405cf2eaeefef8be62775cb31bebe73de0d97
Diefly Oskar <105210849+DieflyCHN@users.noreply.github.com>
2026-04-09 16:20:27 +0200

    Change polt to log scale for better percentage visualization

--- plot.py
+++ plot.py
@@ -1,35 +1,20 @@
 import matplotlib.pyplot as plt
 
 def plot_result(price_history, book_value_history):
-    fig, ax1 = plt.subplots()
+    fig, ax = plt.subplots()
 
-    # --- Original Point ---
-    base_price = price_history[0]
-    base_value = book_value_history[0]
-    scale = base_value / base_price  # ≈ 100
+    ax.plot(price_history, color="red", label="Price")
+    ax.plot(book_value_history, color="green", label="Book Value")
 
-    # --- Left: Price ---
-    ax1.plot(price_history, color="red", label="Price")
-    ax1.set_xlabel("Tick")
-    ax1.set_ylabel("Price", color="red")
-    ax1.tick_params(axis='y', labelcolor="red")
-    ax1.grid(True)
+    ax.set_xlabel("Tick")
+    ax.set_ylabel("Value (log scale)")
+    # Dont use linear anymore, log is better when dealing with two "y"
+    # Just think about percentage instead of difference
+    ax.set_yscale("log")
 
-    # --- Right: Account ---
-    def price_to_value(y):
-        return y * scale
+    ax.grid(True, which="both")
+    ax.legend(loc="upper left")
 
-    def value_to_price(y):
-        return y / scale
-
-    ax2 = ax1.secondary_yaxis('right', functions=(price_to_value, value_to_price))
-    ax2.set_ylabel("Book Value", color="green")
-
-    scaled_value = [v / scale for v in book_value_history]
-    ax1.plot(scaled_value, color="green", label="Book Value")
-
-    ax1.legend(loc="upper left")
-
-    plt.title("Price vs Account Value (Aligned Scale)")
+    plt.title("Price vs Account Value (Log Scale)")
     plt.tight_layout()
     plt.show()
